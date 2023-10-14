36e874023bd0aa381a684fc6e91a8582d11ff8e7
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-14 14:43:09 +0100

    adding product model

---
+++ store/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from store.models import Product
+
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_sku', 'product_slug', 'product_category', 'product_price', 'product_stock',
+                    'is_available', 'created_at', 'updated_at')
+    prepopulated_fields = {'product_slug': ('product_name',)}
+
+
+admin.site.register(Product, ProductAdmin)
---
+++ store/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from category.models import Category
+
+
+# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    product_slug = models.SlugField(max_length=200, unique=True)
+    product_sku = models.CharField(max_length=25, unique=True)
+    product_description = models.CharField(max_length=500, blank=True)
+    product_price = models.IntegerField()
+    product_images = models.ImageField(upload_to='images/products', blank=True)
+    product_stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    # @TODO: the below on_delete will delete the product if the category exists, we should consider changing this
+    #        for example we could add to a review style category and set the product to not be available.
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
