b61fd2c6a94b4849c8ca5693868be254d97e05c4
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-14 14:13:51 +0100

    change category_slug to be a SlugField + slug is prepopulated based on category name

--- category/models.py
+++ category/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    category_slug = models.CharField(max_length=100, unique=True)
+    category_slug = models.SlugField(max_length=100, unique=True)
+    # category_slug = models.CharField(max_length=100, unique=True)
     category_description = models.TextField(max_length=255, blank=True)
     category_image = models.ImageField(upload_to='images/category', blank=True)
 
