0e3bfc0fca61d4aca4d2835c7ef9745e277f777e
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-14 14:07:14 +0100

    allowing for media uploads

--- infiniteShopper/settings.py
+++ infiniteShopper/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
     'infiniteShopper/static',
 ]
 
+# media files config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
