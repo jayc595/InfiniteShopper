a4721af3b0376ad1d4ad0ce2ce772944a0311b3a
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-15 10:50:37 +0100

    counts product collection and displays the amount of products available

--- store/views.py
+++ store/views.py
@@ -5,8 +5,10 @@ from store.models import Product
 # Create your views here.
 def store(request):
     products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
 
     context = {
         'products': products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
