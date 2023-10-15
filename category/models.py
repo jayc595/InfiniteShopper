9ed513d53acfe3c92f95ef0cc2d0151b246bd59f
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-15 11:33:10 +0100

    adding category links to navbar and to store page

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -14,6 +15,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
+
+    def get_slug_url(self):
+        return reverse('products_by_category', args=[self.category_slug])
 
     def __str__(self):
         return self.category_name
