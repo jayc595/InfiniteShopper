4e8c1991ad958374fa84ff466a6b700efea8fb9c
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-16 20:32:30 +0100

    cart and cart item models, starting add to cart functionality

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from store.models import Product
+
 
 # Create your models here.
+
+class Cart(models.Model):
+    quote_id = models.CharField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.quote_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    qty = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from store.models import Product
+from .models import Cart, CartItem
+from django.http import HttpResponse
 
 
 # Create your views here.
+
+def _get_quote_id(request):
+    quote = request.session.session_key
+    if not quote:
+        quote = request.session.create
+    return quote
+
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(quote_id=_get_quote_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            quote_id=_get_quote_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.qty += 1
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            cart=cart,
+            product=product,
+            qty=1,
+        )
+    cart_item.save()
+    # return HttpResponse(cart_item.product)
+    # exit()
+    return redirect('cart')
+
 
 def cart(request):
     return render(request, 'store/cart.html')
