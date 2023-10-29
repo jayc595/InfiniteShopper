ea85207a95803334a637f71b1ca3c080eef7b23b
jayc595 <jay.crocker@hotmail.co.uk>
2023-10-29 19:27:31 +0000

    bug fixing, db refresh

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,5 +1,6 @@
 from cart.models import Cart,CartItem
 from cart.views import _get_quote_id
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def get_cart_item_total(request):
@@ -7,6 +8,6 @@ def get_cart_item_total(request):
         cart = Cart.objects.get(quote_id=_get_quote_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         item_total = cart_items.count()
-    except ObjectNotExist:
+    except Cart.DoesNotExist:
         item_total = 0
     return dict(item_total=item_total)
--- cart/views.py
+++ cart/views.py
@@ -15,6 +15,7 @@ def _get_quote_id(request):
 
 
 def add_to_cart(request, product_id):
+
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(quote_id=_get_quote_id(request))
